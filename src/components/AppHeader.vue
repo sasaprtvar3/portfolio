3dff1c7b680bb162c14ca105be0720e38d2587d7
Saša Prtvar <prtvar11@hotmail.com>
2018-01-18 23:06:56 -0700

    portfolio

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,7 +2,7 @@
   <header>
       <nav class="menu">
      <ul>
-      <li><a href="#/home">Home</a></li>
+      <li><a href="#/">Home</a></li>
       <li><a href="#/about">About</a></li>
       <li><a href="#/contact">Contact</a></li>
      </ul>
